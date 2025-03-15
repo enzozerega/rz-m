7634b6f0c3c9a4e3ea3552055b435a390ab021e7
Enzo Zerega <enzo.zerega@winningtemp.com>
2025-03-15 17:16:26 +0100

    Refactor live.ts to remove defineLive and simplify sanityFetch implementation

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,9 +1,8 @@
-import {defineLive} from 'next-sanity'
 import {client} from './client'
-import {token} from './token'
 
-export const {SanityLive, sanityFetch} = defineLive({
-  client,
-  serverToken: token,
-  browserToken: token,
-})
+const sanityFetch = async ({query, params}: {query: string, params?: object}) => {
+  const data = await client.fetch(query, params)
+  return {data}
+}
+
+export {sanityFetch}
